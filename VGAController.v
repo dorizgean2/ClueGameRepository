a702a6d36c320e6fea3c8872501dfe7665be4106
dorizgean2 <doriz.gean@gmail.com>
2023-12-03 18:03:06 -0500

    vgaController updated (hopefully)

--- VGAController.v
+++ VGAController.v
@@ -6,6 +6,7 @@ module VGAController(
 	input BTNU, 
 	input BTNL, 
 	input BTNR,
+	input BTNC,
 	input [3:0] SW,
 	output hSync, 		// H Sync Signal
 	output vSync, 		// Veritcal Sync Signal
@@ -16,7 +17,7 @@ module VGAController(
 	inout ps2_data);
 	
 	// Lab Memory Files Location
-	localparam FILES_PATH = "C:/Users/dsc54/Desktop/final_project/";
+	localparam FILES_PATH = "C:/Users/jtf45/Desktop/ClueGameRepository-main/Sprites/";
 
 	// Clock divider 100 MHz -> 25 MHz
 	wire clk25; // 25MHz clock
@@ -40,42 +41,45 @@ module VGAController(
 	reg[9:0] square1_y, square2_y, square3_y, square4_y;
 	reg btnd_press, btnu_press, btnl_press, btnr_press;
 	reg[7:0] hold_scan;
-	wire[7:0] to_appear;
 	reg square1, square2, square3, square4;
 	wire data_rdy;
 	wire[7:0] ascii_value, sprite1, sprite2, scan_code;
 	reg square1_move, square2_move, square3_move, square4_move;
-	wire BTND_out, BTNU_out, BTNL_out, BTNR_out, button_out, button_in;
+	wire BTND_out, BTNU_out, BTNL_out, BTNR_out, button_out;
+	wire[31:0] processor_return;
+	reg[31:0] processor_other;
 	
     assign square1_topx = x-(square1_x-20);
     assign square1_topy = y-(square1_y-20);
     assign square2_topx = x-(square2_x-20);
     assign square2_topy = y-(square2_y-20);
     
-	debounce_better_version debouncey(.pb_1(BTND), .clk(clk), .pb_out(BTND_out));
-	debounce_better_version debouncey(.pb_1(BTNU), .clk(clk), .pb_out(BTNU_out));
-	debounce_better_version debouncey(.pb_1(BTNL), .clk(clk), .pb_out(BTNL_out));
-	debounce_better_version debouncey(.pb_1(BTNR), .clk(clk), .pb_out(BTNR_out));
-	assign button_out = BTND_out || BTNU_out || BTNL_out || BTNR_out;
-	Wrapper proc (.clock(clk), .reset(reset), .button_in(button_out), .button_out(button_in));
+	debounce_better_version debouncey1(.pb_1(BTND), .clk(clk), .pb_out(BTND_out));
+	debounce_better_version debouncey2(.pb_1(BTNU), .clk(clk), .pb_out(BTNU_out));
+	debounce_better_version debouncey3(.pb_1(BTNL), .clk(clk), .pb_out(BTNL_out));
+	debounce_better_version debouncey4(.pb_1(BTNR), .clk(clk), .pb_out(BTNR_out));
+	assign button_out = {BTND_out || BTNU_out || BTNL_out || BTNR_out};
+	Wrapper proc (.clock(clk), .reset(reset), .button_in(button_out), .processor_out(processor_return));
     
 	initial begin
-		square1_x = 95;
-		square1_y = 85;
+		square1_x = 400;
+		square1_y = 30;
 		
-		square2_x = 400;
-		square2_y = 400;
+		square2_x = 240;
+		square2_y = 30;
 				
-		square3_x = 510;
-		square3_y = 85;
+		square3_x = 520;
+		square3_y = 150;
 		
-		square4_x = 320;
-		square4_y = 200;
+		square4_x = 180;
+		square4_y = 300;
 		
 		square1_move = 0;
 		square2_move = 0;
 		square3_move = 0;
 		square4_move = 0;
+		
+		processor_other = 32'b0;
 	end
 	
 	
@@ -119,6 +123,21 @@ module VGAController(
 	     
 	end
 	
+	   always @(posedge screenEnd) begin
+            if (square1_move == 1'b1) begin
+                if(BTND == 1'b1) 
+                    square1_y = square1_y + 1;
+                    
+                if(BTNU == 1'b1) 
+                    square1_y = square1_y - 1;
+                    
+                if(BTNL == 1'b1) 
+                    square1_x = square1_x - 1;
+                    
+                if(BTNR == 1'b1) 
+                    square1_x = square1_x + 1;
+	   end
+	end
 	
 		always @(posedge screenEnd) begin
             if (square2_move == 1'b1) begin
@@ -172,6 +191,27 @@ module VGAController(
        end
 	end
 	
+	always @(posedge screenEnd) begin
+	   if (BTND == 1'b1) 
+	       processor_other = 32'b0;
+	   if (BTND == 1'b1) 
+	       processor_other = 32'b1;
+	      
+	end
+	
+	
+	
+    
+	always @(posedge screenEnd) begin
+	   if (square1 == 1'b1) begin
+           if (processor_other == 32'b1)
+                to_appear = sprite_1_data;
+           else 
+                to_appear = 12'd5;
+           end
+    end
+	
+	assign to_appear_sprite_1 = to_appear;
 	
 //	Ps2Interface ps2(ps2_clk, ps2_data, clk, 1'b0, 1'b0, 1'b0, scan_code, data_rdy, busy, error);
 	
@@ -182,7 +222,19 @@ module VGAController(
 	
 //    RAM #(.DATA_WIDTH(7), .ADDRESS_WIDTH(8), .DEPTH(256), .MEMFILE("ascii.mem")) ram_ascii(clk, 1'b0, hold_scan, 1'b0, ascii_value);
 
-  
+//  if ( x == 1 ) 
+//      if(left button)
+//          x = 4 
+//  if (x < 4) 
+//      if (right button)
+//          x = x + 1 
+//      if (left button)
+//          x = x - 1 
+//  if x = 4 
+//      if (right button)
+//          x = 1 
+//  if (btnc) 
+//     wrapper ( clk, reset, button_in = x, processor_return = accusionation correct or not)
 	always @(posedge clk25) begin
 		if (x < square1_x + 10  && y < square1_y + 10 && x > square1_x - 10 && y > square1_y - 10)
             square1 = 1'b1;         
@@ -240,7 +292,7 @@ module VGAController(
 		.DEPTH(PIXEL_COUNT), 				     // Set RAM depth to contain every pixel
 		.DATA_WIDTH(PALETTE_ADDRESS_WIDTH),      // Set data width according to the color palette
 		.ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH),     // Set address with according to the pixel count
-		.MEMFILE({FILES_PATH, "boardgame.mem"})) // Memory initialization
+		.MEMFILE({FILES_PATH, "image.mem"})) // Memory initialization
 	ImageData(
 		.clk(clk), 						 // Falling edge of the 100 MHz clk
 		.addr(imgAddress),					 // Image data address
@@ -254,7 +306,7 @@ module VGAController(
 		.DEPTH(PALETTE_COLOR_COUNT), 		       // Set depth to contain every color		
 		.DATA_WIDTH(BITS_PER_COLOR), 		       // Set data width according to the bits per color
 		.ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH),     // Set address width according to the color count
-		.MEMFILE({FILES_PATH, "boardgame_colors.mem"}))  // Memory initialization
+		.MEMFILE({FILES_PATH, "colors.mem"}))  // Memory initialization
 	ColorPalette(
 		.clk(clk), 							   	   // Rising edge of the 100 MHz clk
 		.addr(colorAddr),					       // Address from the ImageData RAM
@@ -263,8 +315,9 @@ module VGAController(
 	
 
     wire[PIXEL_ADDRESS_WIDTH-1:0] sprite_1_Address, sprite_2_Address, sprite_3_Address, sprite_4_Address;  
-    wire[PALETTE_ADDRESS_WIDTH-1:0] sprite_1_colorAddr, sprite_2_colorAddr, sprite_3_colorAddr, sprite_4_colorAddr;
-    wire[BITS_PER_COLOR-1:0] sprite_1_data, sprite_2_data, sprite_3_data, sprite_4_data;
+    wire[PALETTE_ADDRESS_WIDTH-1:0] sprite_1_colorAddr, sprite_2_colorAddr, sprite_3_colorAddr, sprite_4_colorAddr, dice_prompt_colorAddr;
+    wire[BITS_PER_COLOR-1:0] sprite_1_data, sprite_2_data, sprite_3_data, sprite_4_data, to_appear_sprite_1;
+    reg[BITS_PER_COLOR-1:0] to_appear;
     assign sprite_1_Address = ((x-square1_x-10)) + ((y-square1_y+10)*20);	
     assign sprite_2_Address = ((x-square2_x-10)) + ((y-square2_y+10)*20);	
     assign sprite_3_Address = ((x-square3_x-10)) + ((y-square3_y+10)*20);	
@@ -281,11 +334,15 @@ module VGAController(
     
     RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE("pink_sprite_image.mem")) ram_sprites4(.clk(clk), .wEn(1'b0), .addr(sprite_4_Address), .dataOut(sprite_4_colorAddr));
     RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE("pink_sprite_colors.mem")) ram_sprites_color4(.clk(clk), .wEn(1'b0), .addr(sprite_4_colorAddr), .dataOut(sprite_4_data));
+    
+    // prompts
+//    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE("dice_prompt_image.mem")) dice_prompt(.clk(clk), .wEn(1'b0), .addr(sprite_4_Address), .dataOut(dice_prompt_colorAddr));
+//    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE("dice_prompt_colors.mem")) ram_sprites_color4(.clk(clk), .wEn(1'b0), .addr(sprite_4_colorAddr), .dataOut(sprite_4_data));
 	// Assign to output color from register if active
 	wire[BITS_PER_COLOR-1:0] colorOut; 			  // Output color 
-	wire[BITS_PER_COLOR-1:0] square1_color; 
 	assign colorOut = active ? colorData : 12'd0; // When not active, output black
-	//assign square1_color = active ?  sprite_1_data : 12'd0; // When not active, output black
-	// Quickly assign the output colors to their channels using concatenation
-	assign {VGA_R, VGA_G, VGA_B} = button_in && square1 ? sprite_2_data : square1 ? sprite_1_data : square2 ? sprite_2_data : square3 ? sprite_3_data : square4 ? sprite_4_data : colorOut;
+	// Quickly assign the output colors to their channels using active
+
+	assign {VGA_R, VGA_G, VGA_B} = square1 ? to_appear_sprite_1 : square2 ? sprite_2_data : square3 ? sprite_3_data : square4 ? sprite_4_data : colorOut;
+	//assign {VGA_R, VGA_G, VGA_B} = 12'd0;
 endmodule
