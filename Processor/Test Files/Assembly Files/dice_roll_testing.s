a7aca1aeea7e6ac723335fdbf39681e9ae055790
dorizgean2 <doriz.gean@gmail.com>
2023-12-07 19:44:23 -0500

    right button

--- Processor/Test Files/Assembly Files/dice_roll_testing.s
+++ Processor/Test Files/Assembly Files/dice_roll_testing.s
@@ -2,7 +2,7 @@ main:
 nop					
 nop
 nop
-addi $t2, $zero, 1		# r10 = 1 
+addi $t2, $zero, 1		    # r10 = 1 
 nop
 nop
 nop
@@ -26,19 +26,19 @@ dice_btn_left_press:        # only for dice roll prompt
 nop
 nop
 nop
-lw  $t8, 0($t7)		# r24 = button input
+lw  $in, 0($t7)		        # r24 = button input
 nop
 nop
 nop
-bne $t8, $t2, dice_btn_right_press		# check if right button has been pressed
+bne $in, $t2, dice_btn_right_press		# check if right button has been pressed
 nop                                 
 nop
 nop                                 # if left button has been pressed, then proceed to dice being rolled
-addi $t9, $zero, 1                  # processor output = 1
+addi $out, $zero, 1                  # processor output = 1
 nop
 nop
 nop
-sw $t9, 0($t1)
+sw $out, 0($t1)
 nop
 nop
 nop
@@ -52,11 +52,11 @@ dice_btn_right_press:
 nop
 nop
 nop
-lw $t8, 0($t6)                              # r14 = BTNR
+lw $in, 0($t6)                              # r14 = BTNR
 nop
 nop
 nop
-bne $t8, $t2, dice_btn_left_press         # check if left button has been pressed
+bne $in, $t2, dice_btn_left_press         # check if left button has been pressed
 nop
 nop
 nop          
