e063985a597427cf3822d5f2cc0fc1baaf1ed83c
dorizgean2 <doriz.gean@gmail.com>
2023-12-08 16:51:42 -0500

    new dice roll memory file

--- Processor/Test Files/Assembly Files/dice_roll_testing.s
+++ Processor/Test Files/Assembly Files/dice_roll_testing.s
@@ -12,7 +12,11 @@ bne $s1, $t2, dice_btn_right_press	# check if left button has been pressed
 addi $t9, $zero, 1                  	# processor output = r25 = 1
 sw $t9, 0($t1)				# r9 = processor output = 1
 lw $t0, 0($t1)				# r8 = r9 = 1
-j end_test
+add $s1, $zero, $zero
+add $t8, $zero, $zero
+add $t9, $zero, $zero
+add $t0, $zero, $zero
+j dice_btn_left_press
 
 dice_btn_right_press:
 lw $t8, 0($t6)                           # r14 = BTNR input
@@ -21,7 +25,8 @@ bne $s2, $t2, dice_btn_left_press        # check if right button has been presse
 addi $t9, $zero, 2			 # processor output = r25 = 2
 sw $t9 0($t1)				 # r9 = processor output = 2
 lw $t0, 0($t1)				 # r8 = r9 = 2
+add $s1, $zero, $zero
+add $t8, $zero, $zero
+add $t9, $zero, $zero
+add $t0, $zero, $zero
 j dice_btn_left_press
-
-end_test:
-addi $s0, $zero, 10
