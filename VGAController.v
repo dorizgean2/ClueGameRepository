a2c98f73c945c8ea3c4f6970eef65260f2a30687
dorizgean2 <55556998+dorizgean2@users.noreply.github.com>
2023-12-07 19:49:54 -0500

    Add files via upload

--- VGAController.v
+++ VGAController.v
@@ -2,11 +2,11 @@
 module VGAController(     
 	input clk, 			// 100 MHz System Clock
 	input reset, 		// Reset Signal
-	input BTND, 
-	input BTNU, 
-	input BTNL, 
-	input BTNR,
-	input BTNC,
+	input BTND,         // Debounced BTND
+	input BTNU,         // Debounced BTNU
+	input BTNL,         // Debounced BTNL
+	input BTNR,         // Debounced BTNR
+	input BTNC,         // Debounced BTNC
 	input [3:0] SW,
 	output hSync, 		// H Sync Signal
 	output vSync, 		// Veritcal Sync Signal
@@ -14,7 +14,9 @@ module VGAController(
 	output[3:0] VGA_G,  // Green Signal Bits
 	output[3:0] VGA_B,  // Blue Signal Bits
 	inout ps2_clk,
-	inout ps2_data);
+	inout ps2_data,
+	input from_processor,
+	output to_processor);
 	
 	// Lab Memory Files Location
 	localparam FILES_PATH = "C:/Users/jtf45/Desktop/ClueGameRepository-main/Sprites/";
@@ -31,55 +33,57 @@ module VGAController(
 	// VGA Timing Generation for a Standard VGA Screen
 	localparam 
 		VIDEO_WIDTH = 640,  // Standard VGA Width
-		VIDEO_HEIGHT = 480; // Standard VGA Height
+		VIDEO_HEIGHT = 480, // Standard VGA Height
+		MAX_MOVEMENT = 5,
+		LOWER_Y_BOUNDARY = 30,
+		UPPER_Y_BOUNDARY = 450,
+	    LOWER_X_BOUNDARY = 100,
+	    UPPER_X_BOUNDARY = 520;
 
 	wire active, screenEnd, busy, error;
 	wire[9:0] x, square1_topx, square2_topx;
 	wire[8:0] y, square1_topy, square2_topy;
 
-	reg[9:0] square1_x, square2_x, square3_x, square4_x;
-	reg[9:0] square1_y, square2_y, square3_y, square4_y;
+	reg[9:0] square1_x, square2_x, square3_x, square4_x, dice_prompt_x, dice_data_x;
+	reg[9:0] square1_y, square2_y, square3_y, square4_y, dice_prompt_y, dice_data_y;
 	reg btnd_press, btnu_press, btnl_press, btnr_press;
 	reg[7:0] hold_scan;
-	reg square1, square2, square3, square4;
+	reg square1, square2, square3, square4, roll_dice_ques, show_dice;
 	wire data_rdy;
 	wire[7:0] ascii_value, sprite1, sprite2, scan_code;
 	reg square1_move, square2_move, square3_move, square4_move;
-	wire BTND_out, BTNU_out, BTNL_out, BTNR_out, button_out;
 	wire[31:0] processor_return;
-	reg[31:0] processor_other;
+	assign processor_return = from_processor;
 	
     assign square1_topx = x-(square1_x-20);
     assign square1_topy = y-(square1_y-20);
     assign square2_topx = x-(square2_x-20);
     assign square2_topy = y-(square2_y-20);
     
-	debounce_better_version debouncey1(.pb_1(BTND), .clk(clk), .pb_out(BTND_out));
-	debounce_better_version debouncey2(.pb_1(BTNU), .clk(clk), .pb_out(BTNU_out));
-	debounce_better_version debouncey3(.pb_1(BTNL), .clk(clk), .pb_out(BTNL_out));
-	debounce_better_version debouncey4(.pb_1(BTNR), .clk(clk), .pb_out(BTNR_out));
-	assign button_out = {BTND_out || BTNU_out || BTNL_out || BTNR_out};
-	Wrapper proc (.clock(clk), .reset(reset), .button_in(button_out), .processor_out(processor_return));
-    
+
 	initial begin
 		square1_x = 400;
 		square1_y = 30;
 		
-		square2_x = 240;
-		square2_y = 30;
+		square2_x = 280;
+		square2_y = 440;
 				
 		square3_x = 520;
 		square3_y = 150;
 		
-		square4_x = 180;
-		square4_y = 300;
+		square4_x = 120;
+		square4_y = 340;
+		
+		dice_prompt_x = VIDEO_WIDTH/2;
+		dice_prompt_y = VIDEO_HEIGHT/2;
+				
+		dice_data_x = VIDEO_WIDTH/2;
+		dice_data_y = VIDEO_HEIGHT/2;
 		
 		square1_move = 0;
 		square2_move = 0;
 		square3_move = 0;
 		square4_move = 0;
-		
-		processor_other = 32'b0;
 	end
 	
 	
@@ -103,115 +107,84 @@ module VGAController(
 	     
 	end
 	
-	always @(posedge screenEnd) begin //need to find a way so that the sprites only move one grid when pressed down
-	   if (BTND == 1'b1)
-	       btnd_press = 1'b1;
-	   if (BTND == 1'b0)
-	       btnd_press = 1'b0;       
-	   if (BTNU == 1'b1)
-	       btnu_press = 1'b1;
-	   if (BTNU == 1'b0)
-	       btnu_press = 1'b0;
-	   if (BTNL == 1'b1)
-	       btnl_press = 1'b1; 
-	   if (BTNL == 1'b0)
-	       btnl_press = 1'b0;    
-	   if (BTNR == 1'b1)
-	       btnr_press = 1'b1;
-	   if (BTNR == 1'b0)
-	       btnr_press = 1'b0;
-	     
-	end
-	
 	   always @(posedge screenEnd) begin
             if (square1_move == 1'b1) begin
                 if(BTND == 1'b1) 
-                    square1_y = square1_y + 1;
-                    
+                    if(square1_y + 1 < UPPER_Y_BOUNDARY)
+                        square1_y = square1_y + MAX_MOVEMENT;
                 if(BTNU == 1'b1) 
-                    square1_y = square1_y - 1;
-                    
+                    if(square1_y - 1 > LOWER_Y_BOUNDARY)
+                        square1_y = square1_y - MAX_MOVEMENT;
                 if(BTNL == 1'b1) 
-                    square1_x = square1_x - 1;
-                    
+                    if(square1_x - 1 > LOWER_X_BOUNDARY)
+                        square1_x = square1_x - MAX_MOVEMENT;
                 if(BTNR == 1'b1) 
-                    square1_x = square1_x + 1;
+                    if(square1_x + 1 < UPPER_X_BOUNDARY)
+                        square1_x = square1_x + MAX_MOVEMENT;
 	   end
 	end
 	
 		always @(posedge screenEnd) begin
             if (square2_move == 1'b1) begin
                 if(BTND == 1'b1) 
-                    square2_y = square2_y + 1;
-                    
+                    if(square2_y + 1 < UPPER_Y_BOUNDARY)
+                        square2_y = square2_y + MAX_MOVEMENT;
+                        
                 if(BTNU == 1'b1) 
-                    square2_y = square2_y - 1;
+                    if(square2_y - 1 > LOWER_Y_BOUNDARY)
+                        square2_y = square2_y - MAX_MOVEMENT;
                     
                 if(BTNL == 1'b1) 
-                    square2_x = square2_x - 1;
+                    if(square2_x - 1 > LOWER_X_BOUNDARY)
+                        square2_x = square2_x - MAX_MOVEMENT;
                     
                 if(BTNR == 1'b1) 
-                    square2_x = square2_x + 1;
+                    if(square2_x + 1 < UPPER_X_BOUNDARY)
+                        square2_x = square2_x + MAX_MOVEMENT;
 	   end
 	end
-	
 	
 	
     always @(posedge screenEnd) begin
         if (square3_move == 1'b1) begin
             if(BTND == 1'b1) 
-                square3_y = square3_y + 1;
+                if(square3_y + 1 < UPPER_Y_BOUNDARY)
+                    square3_y = square3_y + MAX_MOVEMENT;
                 
             if(BTNU == 1'b1) 
-                square3_y = square3_y - 1;
+                if(square3_y - 1 > LOWER_Y_BOUNDARY)
+                    square3_y = square3_y - MAX_MOVEMENT;
                 
             if(BTNL == 1'b1) 
-                square3_x = square3_x - 1;
+                if(square3_x - 1 > LOWER_X_BOUNDARY)
+                square3_x = square3_x - MAX_MOVEMENT;
                 
             if(BTNR == 1'b1) 
-                square3_x = square3_x + 1;
+                if(square3_x + 1 < UPPER_X_BOUNDARY)
+                    square3_x = square3_x + MAX_MOVEMENT;
 	   end
-	end
-	
+	end 
 	
 	
 	always @(posedge screenEnd) begin
 	   if (square4_move == 1'b1) begin
             if(BTND == 1'b1) 
-                square4_y = square4_y + 1;
+                if(square4_y + 1 < UPPER_Y_BOUNDARY)
+                    square4_y = square4_y + MAX_MOVEMENT;
                 
             if(BTNU == 1'b1) 
-                square4_y = square4_y - 1;
+                if(square4_y - 1 > LOWER_Y_BOUNDARY)
+                    square4_y = square4_y - MAX_MOVEMENT;
                 
             if(BTNL == 1'b1) 
-                square4_x = square4_x - 1;
+                if(square4_x - 1 > LOWER_X_BOUNDARY)
+                    square4_x = square4_x - MAX_MOVEMENT;
                 
             if(BTNR == 1'b1) 
-                square4_x = square4_x + 1;
+                if(square4_x + 1 < UPPER_X_BOUNDARY)
+                    square4_x = square4_x + MAX_MOVEMENT;
        end
 	end
-	
-	always @(posedge screenEnd) begin
-	   if (BTND == 1'b1) 
-	       processor_other = 32'b0;
-	   if (BTND == 1'b1) 
-	       processor_other = 32'b1;
-	      
-	end
-	
-	
-	
-    
-	always @(posedge screenEnd) begin
-	   if (square1 == 1'b1) begin
-           if (processor_other == 32'b1)
-                to_appear = sprite_1_data;
-           else 
-                to_appear = 12'd5;
-           end
-    end
-	
-	assign to_appear_sprite_1 = to_appear;
 	
 //	Ps2Interface ps2(ps2_clk, ps2_data, clk, 1'b0, 1'b0, 1'b0, scan_code, data_rdy, busy, error);
 	
@@ -256,11 +229,25 @@ module VGAController(
 			square3 = 1'b0;
 	end
 	
-		always @(posedge clk25) begin
+	always @(posedge clk25) begin
 		if (x < square4_x + 10  && y < square4_y + 10 && x > square4_x - 10 && y > square4_y - 10)
             square4 = 1'b1;                
 		else
 			square4 = 1'b0;
+	end
+	
+	always @(posedge clk25) begin
+		if (x < dice_prompt_x + 106 && y < dice_prompt_y + 28 && x > dice_prompt_x - 106 && y > dice_prompt_y - 28)
+            roll_dice_ques = 1'b1;                
+		else
+			roll_dice_ques = 1'b0;
+	end
+	
+	always @(posedge clk25) begin
+		if (x < dice_data_x + 29 && y < dice_data_y + 25 && x > dice_data_x - 29 && y > dice_data_y - 25)
+            show_dice = 1'b1;                
+		else
+			show_dice = 1'b0;
 	end
 
 	VGATimingGenerator #(
@@ -314,35 +301,44 @@ module VGAController(
 		.wEn(1'b0)); 						       // We're always reading
 	
 
-    wire[PIXEL_ADDRESS_WIDTH-1:0] sprite_1_Address, sprite_2_Address, sprite_3_Address, sprite_4_Address;  
-    wire[PALETTE_ADDRESS_WIDTH-1:0] sprite_1_colorAddr, sprite_2_colorAddr, sprite_3_colorAddr, sprite_4_colorAddr, dice_prompt_colorAddr;
-    wire[BITS_PER_COLOR-1:0] sprite_1_data, sprite_2_data, sprite_3_data, sprite_4_data, to_appear_sprite_1;
+    wire[PIXEL_ADDRESS_WIDTH-1:0] sprite_1_Address, sprite_2_Address, sprite_3_Address, sprite_4_Address, dice_roll_Address, dice_Address;  
+    wire[PALETTE_ADDRESS_WIDTH-1:0] sprite_1_colorAddr, sprite_2_colorAddr, sprite_3_colorAddr, sprite_4_colorAddr, dice_prompt_colorAddr, dice_colorAddr;
+    wire[BITS_PER_COLOR-1:0] sprite_1_data, sprite_2_data, sprite_3_data, sprite_4_data, to_appear_sprite_1, dice_prompt_data, dice_data;
     reg[BITS_PER_COLOR-1:0] to_appear;
+    
     assign sprite_1_Address = ((x-square1_x-10)) + ((y-square1_y+10)*20);	
     assign sprite_2_Address = ((x-square2_x-10)) + ((y-square2_y+10)*20);	
     assign sprite_3_Address = ((x-square3_x-10)) + ((y-square3_y+10)*20);	
     assign sprite_4_Address = ((x-square4_x-10)) + ((y-square4_y+10)*20);	
+    assign dice_roll_Address = ((x-dice_prompt_x-106)) + ((y-dice_prompt_y+28)*212);
+    assign dice_Address = ((x-dice_data_x-29)) + ((y-dice_data_y+148)*58);
 
-    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE("red_sprite_image.mem")) ram_sprites1(.clk(clk), .wEn(1'b0), .addr(sprite_1_Address), .dataOut(sprite_1_colorAddr));
-    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE("red_sprite_colors.mem")) ram_sprites_color1(.clk(clk), .wEn(1'b0), .addr(sprite_1_colorAddr), .dataOut(sprite_1_data));
+    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE({FILES_PATH, "red_sprite_image.mem"})) ram_sprites1(.clk(clk), .wEn(1'b0), .addr(sprite_1_Address), .dataOut(sprite_1_colorAddr));
+    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE({FILES_PATH, "red_sprite_colors.mem"})) ram_sprites_color1(.clk(clk), .wEn(1'b0), .addr(sprite_1_colorAddr), .dataOut(sprite_1_data));
     
-    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE("purple_sprite_image.mem")) ram_sprites2(.clk(clk), .wEn(1'b0), .addr(sprite_2_Address), .dataOut(sprite_2_colorAddr));
-    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE("purple_sprite_colors.mem")) ram_sprites_color2(.clk(clk), .wEn(1'b0), .addr(sprite_2_colorAddr), .dataOut(sprite_2_data));
+    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE({FILES_PATH,"purple_sprite_image.mem"})) ram_sprites2(.clk(clk), .wEn(1'b0), .addr(sprite_2_Address), .dataOut(sprite_2_colorAddr));
+    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE({FILES_PATH, "purple_sprite_colors.mem"})) ram_sprites_color2(.clk(clk), .wEn(1'b0), .addr(sprite_2_colorAddr), .dataOut(sprite_2_data));
    
-    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE("green_sprite_image.mem")) ram_sprites3(.clk(clk), .wEn(1'b0), .addr(sprite_3_Address), .dataOut(sprite_3_colorAddr));
-    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE("green_sprite_colors.mem")) ram_sprites_color3(.clk(clk), .wEn(1'b0), .addr(sprite_3_colorAddr), .dataOut(sprite_3_data));
+    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE({FILES_PATH, "green_sprite_image.mem"})) ram_sprites3(.clk(clk), .wEn(1'b0), .addr(sprite_3_Address), .dataOut(sprite_3_colorAddr));
+    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE({FILES_PATH, "green_sprite_colors.mem"})) ram_sprites_color3(.clk(clk), .wEn(1'b0), .addr(sprite_3_colorAddr), .dataOut(sprite_3_data));
     
-    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE("pink_sprite_image.mem")) ram_sprites4(.clk(clk), .wEn(1'b0), .addr(sprite_4_Address), .dataOut(sprite_4_colorAddr));
-    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE("pink_sprite_colors.mem")) ram_sprites_color4(.clk(clk), .wEn(1'b0), .addr(sprite_4_colorAddr), .dataOut(sprite_4_data));
+    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE({FILES_PATH, "pink_sprite_image.mem"})) ram_sprites4(.clk(clk), .wEn(1'b0), .addr(sprite_4_Address), .dataOut(sprite_4_colorAddr));
+    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE({FILES_PATH, "pink_sprite_colors.mem"})) ram_sprites_color4(.clk(clk), .wEn(1'b0), .addr(sprite_4_colorAddr), .dataOut(sprite_4_data));
     
     // prompts
-//    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH), .DEPTH(400), .MEMFILE("dice_prompt_image.mem")) dice_prompt(.clk(clk), .wEn(1'b0), .addr(sprite_4_Address), .dataOut(dice_prompt_colorAddr));
-//    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE("dice_prompt_colors.mem")) ram_sprites_color4(.clk(clk), .wEn(1'b0), .addr(sprite_4_colorAddr), .dataOut(sprite_4_data));
+    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(14), .DEPTH(212*58), .MEMFILE({FILES_PATH, "dice_prompt_image.mem"})) dice_prompt(.clk(clk), .wEn(1'b0), .addr(dice_roll_Address), .dataOut(dice_prompt_colorAddr));
+    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE({FILES_PATH, "dice_prompt_colors.mem"})) dice_prompt_colors(.clk(clk), .wEn(1'b0), .addr(dice_prompt_colorAddr), .dataOut(dice_prompt_data));
+    
+    RAM #(.DATA_WIDTH(PALETTE_ADDRESS_WIDTH), .ADDRESS_WIDTH(15), .DEPTH(295*58), .MEMFILE({FILES_PATH, "dice_image.mem"})) dice_image(.clk(clk), .wEn(1'b0), .addr(dice_Address), .dataOut(dice_colorAddr));
+    RAM #(.DATA_WIDTH(12), .ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH), .DEPTH(256), .MEMFILE({FILES_PATH, "dice_colors.mem"})) dice_image_colors(.clk(clk), .wEn(1'b0), .addr(dice_colorAddr), .dataOut(dice_data));
 	// Assign to output color from register if active
 	wire[BITS_PER_COLOR-1:0] colorOut; 			  // Output color 
+	
 	assign colorOut = active ? colorData : 12'd0; // When not active, output black
+//	wire random;
+//	assign random = 1'b1;
 	// Quickly assign the output colors to their channels using active
 
-	assign {VGA_R, VGA_G, VGA_B} = square1 ? to_appear_sprite_1 : square2 ? sprite_2_data : square3 ? sprite_3_data : square4 ? sprite_4_data : colorOut;
-	//assign {VGA_R, VGA_G, VGA_B} = 12'd0;
+	//assign {VGA_R, VGA_G, VGA_B} = roll_dice_ques ? dice_prompt_data : square1 ? sprite_1_data : square2 ? sprite_2_data : square3 ? sprite_3_data : square4 ? sprite_4_data : colorOut;
+	assign {VGA_R, VGA_G, VGA_B} = show_dice ? dice_data : colorOut;
 endmodule
