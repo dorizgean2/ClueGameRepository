531ba792b43b680a2b02080f85d8d270f6ffd251
GamingProphs <gamingprophs@gmail.com>
2023-12-08 20:46:03 -0500

    merge changes

--- Processor/Test Files/Assembly Files/dice_roll_testing.s
+++ Processor/Test Files/Assembly Files/dice_roll_testing.s
@@ -6,6 +6,7 @@ addi $t6, $zero, 4000       		# r14 = 4000 (BTNR)
 addi $t1, $zero, 2000			# r9 = 2000 (processor_output)
 
 dice_btn_left_press:        		# only for dice roll prompt
+<<<<<<< Updated upstream
 lw  $t8, 0($t7)		        	# r24 = BTNL input  
 add $s1, $t8, $zero
 bne $s1, $t2, dice_btn_right_press	# check if left button has been pressed                                 	
@@ -28,3 +29,16 @@ addi $s0, $zero, 10
 
 end_test_right:
 addi $s3, $zero, 5
+=======
+lw  $in, 0($t7)		        	# r24 = BTNL input  
+bne $in, $t2, dice_btn_right_press	# check if left button has been pressed                                 	
+addi $out, $zero, 1                  	# processor output = r25 = 1
+sw $out, 0($t1)				# r9 = processor output = 1
+
+dice_btn_right_press:
+lw $in, 0($t6)                           # r14 = BTNR input
+bne $in, $t2, dice_btn_left_press        # check if right button has been pressed          
+addi $out, $zero, 2			 # processor output = r25 = 2
+sw $out 0($t1)				 # r9 = processor output = 2
+j dice_btn_left_press
+>>>>>>> Stashed changes
