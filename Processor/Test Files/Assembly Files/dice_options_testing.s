4186f7be516aa4d996f9b66ff1746254e80b7aee
dorizgean2 <doriz.gean@gmail.com>
2023-12-11 06:05:21 -0500

    i need a nap

--- Processor/Test Files/Assembly Files/dice_options_testing.s
+++ Processor/Test Files/Assembly Files/dice_options_testing.s
@@ -25,14 +25,13 @@ j dice_roll_prompt
 
 dice_btn_right_press:
 addi $t6, $zero, 4000       # r14 = 4000 (BTNR)
-addi $t3, $zero, 8000            # r12 = 7000 (Red Button)
+addi $t3, $zero, 8000            # r12 = 8000 (Red Button)
 add $t8, $zero, $zero
 lw $t8, 0($t6)
 add $s2, $t8, $zero
 bne $s2, $t2, dice_btn_left_press         # check if left button has been pressed
 addi $t9, $zero, 200                      # accuse someone prompts up
 sw $t9, 0($t1)
-lw $t0, 0($t1)
 add $t8, $zero, $zero
 read_red_button:
 nop
@@ -53,7 +52,6 @@ add $s3, $t8, $zero
 bne $s3, $t2, btn_down_press
 addi $t9, $zero, 2          # up button has been pressed -> processor_output = 1
 sw $t9, 0($t1)              
-lw $t0, 0($t1)
 j accusations
 
 btn_down_press:
@@ -61,10 +59,9 @@ addi $t4, $zero, 6000      # r12 = 6000 (BTND)
 add $t8, $zero, $zero
 lw $t8, 0($t4)          # check if down button has been pressed
 add $s4, $t8, $zero 
-bne $s4, $t2, check_directions_button
+bne $s4, $t2, btn_left_press
 addi $t9, $zero, 3      # down button has been pressed -> processor_t9put = 2
 sw $t9, 0($t1)
-lw $t0, 0($t1)
 j accusations
 
 btn_left_press:
@@ -75,7 +72,6 @@ add $s5, $t8, $zero
 bne $s5, $t2, btn_right_press
 addi $t9, $zero, 4      # left button has been pressed -> processor_t9put = 3
 sw $t9, 0($t1)
-lw $t0, 0($t1)
 j accusations
 
 btn_right_press:
@@ -86,7 +82,6 @@ add $s6, $t8, $zero
 bne $s6, $t2, check_directions_button        # goes back to checkt8g buttons
 addi $t9, $zero, 5      # right button has been pressed -> processor_t9put = 4
 sw $t9, 0($t1)
-lw $t0, 0($t1)
 j accusations 
 
 accusations:                            
