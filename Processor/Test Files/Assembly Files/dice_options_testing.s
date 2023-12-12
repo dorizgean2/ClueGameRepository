3ebbfd40b5efaa44de25681050fdee1db07af396
dorizgean2 <doriz.gean@gmail.com>
2023-12-12 07:21:07 -0500

    slight changes to the assembly

--- Processor/Test Files/Assembly Files/dice_options_testing.s
+++ Processor/Test Files/Assembly Files/dice_options_testing.s
@@ -1,101 +1,122 @@
 main:
-addi $t2, $zero, 1		# r10 = 1 
-addi $t1, $zero, 2000		# r9 = 2000 (processor_input)
+addi $t2, $zero, 1		    # r10 = 1 
+addi $t1, $zero, 2000		# r9 = 2000 (processor_output)
+
 rng $s7, $zero, $zero       # killer's identity is randomized
-addi $t9, $s7, 100          # room selection
-addi $s0, $zero, 304
-sw $t9, 0($t1)
+addi $t9, $s7, 100          # room, killer, and weapon is selected
+sw $t9, 0($t1)              # outputs t9 or room, killer, weapon selection
+
+addi $s0, $zero, 304        # s0 set to be max turns
 addi $t0, $zero, 300        # t0 = turns {300, 301, 302, 303}
-add $t9, $t0, $zero
-sw $t9, 0($t1)
+
 
 dice_roll_prompt:
-add $t9, $t0, $zero
-sw $t9, 0($t1) 
-addi $t9, $zero, 201
+
+addi $t9, $zero, 201                    # processor saves 201 = "dice roll?" prompt
+sw $t9, 0($t1)                          # processor outputs "dice roll?" prompt
+
+dice_btn_left_press:                    # check if left button is pressed
+add $s1, $zero, $zero                   # set s1 = 0
+add $t8, $zero, $zero                   # set t8 = button input = 0
+addi $t7, $zero, 3000                   # set t7 = 3000 (BTNL)
+
+add $t9, $t0, $zero                     # processor saves current turn 
+sw $t9, 0($t1)                          # processsor outputs current turn
+
+lw  $t8, 0($t7)		                    # t8 = button left input
+add $s1, $t8, $zero                     # set s1 = button left input
+
+bne $s1, $t2, dice_btn_right_press		# check if left button has been pressed 
+
+addi $t9, $zero, 199                    
 sw $t9, 0($t1)
 
-dice_btn_left_press:        # only for dice roll prompt
-add $s1, $zero, $zero
-addi $t7, $zero, 3000       # r15 = 3000 (BTNL)
-add $t8, $zero, $zero
-sw $t9, 0($t1)
-lw  $t8, 0($t7)		        # r24 = button output
-add $s1, $t8, $zero
-bne $s1, $t2, dice_btn_right_press		# check if right button has been pressed 
-addi $t9, $zero, 199
-sw $t9, 0($t1)
 nop
 nop
-nop                                             
-rng $t9, $zero, $zero                  # processor in = random generator
+nop                
+
+rng $t9, $zero, $zero                   # processor in = random generator
+
 nop
 nop
 nop
-sw $t9, 0($t1)
+
+sw $t9, 0($t1)                          # processor output random number
+
 nop
 nop
 nop
-addi $s2, $zero, 301
-bne $t0, $s2, movement1
-addi $s2, $zero, 302
-bne $t0, $s2, movement2
-addi $s2, $zero, 303
-bne $t0, $s2, movement3
-j movement4
+
+j movement
 
 dice_btn_right_press:
-addi $t6, $zero, 4000               # r14 = 4000 (BTNR)
-addi $t3, $zero, 7000            # r12 = 7000 (Red Button)
-add $t8, $zero, $zero
-lw $t8, 0($t6)
-add $s2, $t8, $zero
-bne $s2, $t2, dice_btn_left_press         # check if left button has been pressed
-addi $t9, $zero, 200                      # ACCUSE prompts up
-sw $t9, 0($t1)
-add $t8, $zero, $zero
+addi $t6, $zero, 4000                   # t6 = 4000 (BTNR)
+addi $t3, $zero, 7000                   # t3 = 7000 (Red Button)
+
+add $t8, $zero, $zero                   # t8 = 0
+lw $t8, 0($t6)                          # t8 = button right input
+
+add $s2, $t8, $zero                     # s2 = button right input
+bne $s2, $t2, dice_btn_left_press       # check if right button has been pressed
+
+addi $t9, $zero, 200                    # save ACCUSE prompt to processor output
+sw $t9, 0($t1)                          # processor outputs accuse prompt
+
+add $t8, $zero, $zero                   # t8 = 0
+
 read_red_button:
 nop
 nop
 nop 
-lw $t8, 0($t3)                  # read red button
-add $s3, $t8, $zero
+lw $t8, 0($t3)                          # read red button
+add $s3, $t8, $zero                     # save red button input to s3
 nop
+nop     
 nop
-nop
-bne $s3, $t2, read_red_button
-addi $t9, $zero, 202
-sw $t9, 0($t1)
 
-check_directions_button:
-addi $t5, $zero, 5000       # r13 = 5000 (BTNU)
-add $s3, $zero, $zero
-add $t8, $zero, $zero
-lw  $t8, 0($t5)           # check if up button has been pressed
-add $s3, $t8, $zero
+bne $s3, $t2, read_red_button           # check if s2 = 1 (or button is pressed)
+
+addi $t9, $zero, 202                    # save end accuse someone (202) to processor output
+sw $t9, 0($t1)                          # processor outputs end accuse
+
+check_directions_button:                
+addi $t5, $zero, 5000                   # t5 = 5000 (BTNU)
+add $s3, $zero, $zero                   # s3 = 0
+
+add $t8, $zero, $zero                   # t8 = 0
+lw  $t8, 0($t5)                         # check if up button has been pressed
+
+add $s3, $t8, $zero                     # save button input to s3
+
 bne $s3, $t2, btn_down_press
-addi $t9, $zero, 700          # up button has been pressed -> processor_output = 1
-sw $t9, 0($t1)              
-j accuse_weapon_prompt
+
+addi $t9, $zero, 700                    # up button has been pressed -> processor_output = 700
+sw $t9, 0($t1)                          # processor outputs 700
+j accuse_weapon_prompt                  
 
 btn_down_press:
-addi $t4, $zero, 6000      # r12 = 6000 (BTND)
+addi $t4, $zero, 6000                   # r12 = 6000 (BTND)
 add $t8, $zero, $zero
-lw $t8, 0($t4)          # check if down button has been pressed
+lw $t8, 0($t4)                          # check if down button has been pressed
+
 add $s3, $t8, $zero 
 bne $s3, $t2, btn_left_press
-addi $t9, $zero, 701      # down button has been pressed -> processor_t9put = 2
+
+addi $t9, $zero, 701                    # down button has been pressed -> processor_output = 701
 sw $t9, 0($t1)
 j accuse_weapon_prompt
 
 btn_left_press:
-addi $t7, $zero, 3000       # r15 = 3000 (BTNL)
+addi $t7, $zero, 3000                   # r15 = 3000 (BTNL)
 add $t8, $zero, $zero
 lw $t8, 0($t7)
+
 add $s3, $t8, $zero
 bne $s3, $t2, btn_right_press
-addi $t9, $zero, 702      # left button has been pressed -> processor_t9put = 3
+
+addi $t9, $zero, 702                # left button has been pressed -> processor_output = 702
 sw $t9, 0($t1)
+
 j accuse_weapon_prompt
 
 btn_right_press:
@@ -108,70 +129,29 @@ addi $t9, $zero, 703      # right button has been pressed -> processor_t9put = 4
 sw $t9, 0($t1)
 j accuse_weapon_prompt  
 
-movement1:
-add $t8, $zero, $zero
-addi $s3, $zero, 1000        # from VGA
+movement:
 
-check1_movements:                # continuously read from vga until it has reached max movement numbers
-add $t8, $zero, $zero
+addi $s3, $zero, 1000           # s3 = 1000 -> data from VGA
+
+check_movements:                # continuously read from vga until it has reached max movement numbers
+add $t8, $zero, $zero           # t8 = processor input = 0
 lw $t8, 0($s3)      
-bne $t8, $zero, check1_movements       # update next player information
+add $s6, $t8, $zero
 
-update_turn1:
-addi $t0, $zero, 301
-add $t9, $t0, $zero
-sw $t9, 0($t1)
-j dice_roll_prompt
+blt $zero, $s6, check_movements       # update next player information
 
-movement2:
-add $t8, $zero, $zero
-addi $s3, $zero, 1000        # from VGA
+update_turn:
+addi $t0, $t0, 1                      # t0 = set turn = turn + 1
 
-check2_movements:                # continuously read from vga until it has reached max movement numbers
-add $t8, $zero, $zero
-lw $t8, 0($s3)      
-bne $t8, $zero, check2_movements       # update next player information
+add $t9, $t0, $zero                   # save current turn in processor
+sw $t9, 0($t1)                        # output current turn to VGA
 
-update_turn2:
-addi $t0, $zero, 302
-add $t9, $t0, $zero
-sw $t9, 0($t1)
-j dice_roll_prompt
-
-movement3:
-add $t8, $zero, $zero
-addi $s3, $zero, 1000        # from VGA
-
-check3_movements:                # continuously read from vga until it has reached max movement numbers
-add $t8, $zero, $zero
-lw $t8, 0($s3)      
-bne $t8, $zero, check3_movements       # update next player information
-
-update_turn3:
-addi $t0, $zero, 303
-add $t9, $t0, $zero
-sw $t9, 0($t1)
-j dice_roll_prompt
-
-movement4:
-add $t8, $zero, $zero
-addi $s3, $zero, 1000        # from VGA
-
-check4_movements:                # continuously read from vga until it has reached max movement numbers
-add $t8, $zero, $zero
-lw $t8, 0($s3)      
-bne $t8, $zero, check4_movements       # update next player information
-
-update_turn4:
-addi $t0, $zero, 300
-add $t9, $t0, $zero
-sw $t9, 0($t1)
 j dice_roll_prompt
 
 
 accuse_weapon_prompt:
 addi $t9, $zero, 203            # accusation has been made
-sw $t9, 0($$t1)
+sw $t9, 0($t1)
 add $t8, $zero, $zero
 add $s4, $zero, $zero
 add $s5, $zero, $zero
